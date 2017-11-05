8e4d0be7897f8ba7b1789bb49d718f5111f6d322
Tomek <java-tomek@users.noreply.github.com>
2017-11-05 15:36:54 +0100

    user model class

---
+++ src/main/java/com/tomek/hibernate/model/User.java
@@ -0,0 +1,25 @@
+package com.tomek.hibernate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String surname;
+}
