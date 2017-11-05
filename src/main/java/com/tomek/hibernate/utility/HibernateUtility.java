189c6f2567009911b769aac53abb8566d34a4505
Tomek <java-tomek@users.noreply.github.com>
2017-11-05 15:32:46 +0100

    hibernate utility

---
+++ src/main/java/com/tomek/hibernate/utility/HibernateUtility.java
@@ -0,0 +1,21 @@
+package com.tomek.hibernate.utility;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtility {
+    private final static SessionFactory sf = new Configuration().configure().buildSessionFactory();
+    private static Session session = sf.openSession();
+
+    private HibernateUtility() {
+    }
+
+    public static Session getHibernateSession() {
+        if (session == null) {
+            session = (Session) new HibernateUtility();
+        }
+        return session;
+    }
+
+}
